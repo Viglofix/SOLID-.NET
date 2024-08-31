8138f1a2b24351cee5c76bf0fb2d394d572acdba
viglofix <viglofix@protonmail.com>
2024-08-31 16:37:01 +0200

    OpenClosedPrinciplePassed

--- OpenClosedPrinciple/Program.cs
+++ OpenClosedPrinciple/Program.cs
@@ -1,65 +1,129 @@
-﻿public class Footballer
+﻿using System.Reflection.Metadata;
+
+public class Footballer
 {
     public string Name { get; private set; }
     public int Age { get; private set; }
-    public string Role { get; private set; }
+    private IFootballer _role;
+    public IFootballer Role { 
+    get {
+        return _role;
+    }
+    set {
+        _role = value;
+    }
+    }
 
-    public Footballer(string name, int age, string role)
+    public Footballer(string name, int age, IFootballer role)
     {
         Name = name;
         Age = age;
         Role = role;
     }
 }
-public class Basketballer 
-{
-    public string Name { get; private set; }
-    public int Age { get; private set; }
-    public string Role { get; private set; }
-
-    public Basketballer(string name, int age, string role)
-    {
-        Name = name;
-        Age = age;
-        Role = role;
+public interface IFootballer {
+    public string GetRole();
+}
+public class GoalKeeper : IFootballer {
+    public string GetRole(){
+        return "GoalKeeper";
     }
 }
-public interface IPlayerAgent<T> {
-    public void GetPlayerRole(T player);
+public class ForwardRole : IFootballer {
+    public string GetRole(){
+        return "Forward";
+    }
 }
-public class FootballerAgent : IPlayerAgent<Footballer> {
-    public void GetPlayerRole(Footballer footballer)
+public class DefenderRole  : IFootballer {
+    public string GetRole(){
+        return "DefenderRole";
+    }
+}
+public class MidfieldRole : IFootballer {
+    public string GetRole(){
+        return "MidfieldRole";
+    }
+}
+
+/// <summary>
+/// Section of the another example
+/// </summary>
+/// 
+
+public interface IDiscountSize {
+    public double GetPrice(double price);
+}
+public class TeenPercentPriceDiscount : IDiscountSize
+{
+    public double GetPrice(double price)
     {
-        // switch should be turned into map or some kind of pattern 
-        // filter to implement because of the bunch of data to throw away
+        return price * 0.9;
+    }
+}
 
+public class TwentyPercentPriceDiscount : IDiscountSize
+{
+    public double GetPrice(double price)
+    {
+        return price * 0.8;
+    }
+}
+public class ThirtyPercentPriceDiscount : IDiscountSize
+{
+    public double GetPrice(double price)
+    {
+        return price * 0.7;
+    }
+}
+public class FiftyPercentPriceDiscount : IDiscountSize
+{
+    public double GetPrice(double price)
+    {
+        return price * 0.5;
+    }
+}
 
-        switch (footballer.Role)
-        {
-            case "goalkeeper":
-                Console.WriteLine($"The footballer, {footballer.Name} is a goalkeeper");
-                break;
-            case "defender":
-                Console.WriteLine($"The footballer, {footballer.Name} is a defender");
-                break;
-            case "midfielder":
-                Console.WriteLine($"The footballer, {footballer.Name} is a midfielder");
-                break;
-            case "forward":
-                Console.WriteLine($"The footballer, {footballer.Name} plays in the forward line");
-                break;
-            default:
-                throw new ArgumentException($"Unsupported role type: {footballer.Name}");
+public class PriceCalculator
+{
+    public double CalculatePrice(double price, IDiscountSize size)
+    {
+       return size.GetPrice(price);
+    }
+}
+public class DiscountMapperPriceCalculator {
+    public double? CalculatePriceMap(double price, string size, IDictionary<string,double> collectionOfDiscounts){
+        double? sizeOfDiscount = collectionOfDiscounts[size];
+        if(sizeOfDiscount is null){
+            throw new NullReferenceException("Please put the appropriate value in the size of the discount");
         }
-   }
+        return price * sizeOfDiscount;
+    }
 }
-
 internal class Program
 {
     private static void Main()
     {
-        Footballer kante = new Footballer("Ngolo Kante", 31, "midfielder");
-        Footballer hazard = new Footballer("Eden Hazard", 32, "forward");
+        Dictionary<string,double> collectionOfDiscounts = new Dictionary<string, double>(){
+        {"10",0.9},
+        {"20",0.8},
+        {"30",0.7},
+        {"50",0.5},
+        };
 
+        Dictionary<string,Footballer> map = new Dictionary<string, Footballer>()
+        {
+            {"One", new Footballer("Eden Hazard", 32, new ForwardRole())},
+            {"Two", new Footballer("Joseph Kurtz", 39, new MidfieldRole())},
+            {"Three", new Footballer("Joseph Kurtz", 39, new MidfieldRole())}
+        };
+
+        foreach(var item in map){
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+        DiscountMapperPriceCalculator discountMapperPriceCalculator = new DiscountMapperPriceCalculator();
+        Console.WriteLine(priceCalculator.CalculatePrice(100.00, new FiftyPercentPriceDiscount()));
+        Console.WriteLine(discountMapperPriceCalculator.CalculatePriceMap(100.00, "30",collectionOfDiscounts));
     }
 }
---
+++ OpenClosedPrinciple2/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace LogTypeNamespace;
+public enum LogType
+{
+    File, Database
+}
+
+public class Logger
+{
+    private readonly LogType _logType;
+
+    public Logger(LogType logType)
+    {
+        _logType = logType;
+    }
+
+    public void Log(string message)
+    {
+        /* switch (_logType)
+        {
+            case LogType.File:
+                throw new NotImplementedException();
+            case LogType.Database:
+                throw new NotImplementedException();
+            default:
+                throw new Exception("Unexpected log type!");
+        } */
+        
+    }
+}
+public interface ILogger {
+    public void LogType();
+}
+public class LoggerFile : ILogger
+{
+    public void LogType()
+    {
+        Console.WriteLine("logs has been saved to file...");
+    }
+}
+public class LoggerDatabase : ILogger
+{
+    public void LogType()
+    {
+        Console.WriteLine("logs has been saved to database...");
+    }
+}
+public class LoggerFax : ILogger
+{
+    public void LogType()
+    {
+        Console.WriteLine("logs has been saved to fax...");
+    }
+}
+
+
+public class LoggManager {
+    private readonly ILogger _loggManager;
+    public LoggManager(ILogger logger)
+    {
+        _loggManager = logger;
+    }
+    public void SaveLogs(){
+        _loggManager.LogType();
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LoggManager loggManager = new LoggManager(new LoggerDatabase());
+        loggManager.SaveLogs();
+    }
+}
